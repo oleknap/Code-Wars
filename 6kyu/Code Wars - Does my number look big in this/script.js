c706b29084753b0afdf89331d5ef3baed771fcb9
oleknap <oleknapior@gmail.com>
2023-10-23 18:23:32 +0200

    New 6kyu challenge completed

---
+++ 6kyu/Code Wars - Does my number look big in this/script.js
@@ -0,0 +1,56 @@
+// A Narcissistic Number (or Armstrong Number) is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
+
+// For example, take 153 (3 digits), which is narcissistic:
+
+//     1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+// and 1652 (4 digits), which isn't:
+
+//     1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+// The Challenge:
+
+// Your code must return true or false (not 'true' and 'false') depending upon whether the given number is a Narcissistic number in base 10.
+
+// This may be True and False in your language, e.g. PHP.
+
+// Error checking for text strings or other invalid inputs is not required, only valid positive non-zero integers will be passed into the function.
+
+
+// My solution
+function narcissistic(value) {
+    const digits = value.toString().split('').map(Number);
+    let sum = 0;
+    for(element of digits){
+        sum+=Math.pow(element,digits.length);
+    }
+    if(sum===value){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+  }
+
+console.log(narcissistic(153))
+// New solution that i learned
+
+// testing
+function sumOfDigits(value){
+    return ('' + value).split('').map(Number).reduce(function(a,c){
+        return a+c;
+    }, 0);
+}
+
+console.log(sumOfDigits(1537));
+
+// solution
+function narcissistic2(value){
+    return ('' + value).split("").map(Number).reduce(function(a,c){
+        return a+Math.pow(c, ('' + value).length);
+    },0)===value;
+}
+console.log(narcissistic2(153));
+
+
+
+
